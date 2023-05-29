3924a4a2195af5d61d810b96a1c824b9b531f07f
Gabriel Gamboa <gcarlos.gabriel4@gmail.com>
2023-05-28 22:27:07 -0400

    28-05 22:26
    commit de rigurosidad

--- assets/js/Presupuesto.js
+++ assets/js/Presupuesto.js
@@ -1,79 +1,73 @@
 /**
  * Clase Presupuesto
  * - almacena los valores ingresados a PRESUPUESTO APP
+ * - cada ingreso de saldo o gasto queda almacenado en un objeto map
  * - metodos similares a un CRUD
- * - propiedad ultimoIdGasto permite manipular cada gasto por si ID
+ * - 
  */
 
 class Presupuesto{
     constructor(){
-        this.presupuestoTotal = [];
-        this.listaGastos = [];
-        this.ultimoPresupuestoTotal = 0;
-        this.ultimoGastoTotal = 0
-        this.ultimaDiferenciaTotal = 0
-        this.ultimoIdGasto = 0
+        this.saldos = new Map();
+        this.gastos = new Map();
+        this.contadorGastos = 1
     }
-    agregarPresupuesto(monto){
-        if (monto >= 0){
-            this.presupuestoTotal.push(monto);
-            return monto;
+    agregarSaldos(monto) {
+        if (typeof monto !== 'number' || monto <= 0) {
+            throw new Error('El monto del saldo debe ser mayor a 0.');
         }
-        return NaN;
+        let id = this.saldos.size + 1
+        this.saldos.set(id, { monto });
+        return true
     }
-    sumarPresupuesto(arr = this.presupuestoTotal) {
-        const sumaPresupuesto = arr.reduce((total, monto) => total + monto, 0);
-        this.ultimoPresupuestoTotal = parseInt(sumaPresupuesto);
-        return parseInt(sumaPresupuesto);
-    }
-    agregarGasto(nombre,monto){
-        if(nombre.length > 0 && monto >= 0){
-            const gasto = {
-                id: this.ultimoIdGasto,
-                nombre: nombre,
-                monto: monto
-            }
-            this.ultimoIdGasto++;
-            this.listaGastos.push(gasto);
-            return gasto;
+
+    agregarGasto(nombre, monto) {
+        if (typeof monto !== 'number' || monto <= 0) {
+            throw new Error('El monto del gasto debe ser MAYOR A ');
         }
-        return null;
-    }
-    sumarGastos(arr = this.listaGastos){
-        const sumaMontos = arr.reduce((total, gasto) => total + gasto.monto, 0);
-        this.ultimoGastoTotal = parseInt(sumaMontos)
-        return parseInt(sumaMontos)
-    }
-    calcularDiferencia(presupuesto = this.ultimoPresupuestoTotal, gastos = this.ultimoGastoTotal ){
-        const diferencia = presupuesto - gastos;
-        this.ultimaDiferenciaTotal = diferencia
-        return parseInt(diferencia)
-    }
-    buscarGasto(id) {
-        const gastoEncontrado = this.listaGastos.find(gasto => gasto.id === id);
-        if (gastoEncontrado) {
-            return gastoEncontrado;
-        } else {
-            return null;
+        if (typeof nombre !== 'string' || nombre.trim().length === 0) {
+            throw new Error('El nombre del gasto NO PUEDE ESTAR VACIO.');
         }
+        const id = this.contadorId; 
+        const nuevoGasto = { nombre, monto };
+        this.gastos.set(id, nuevoGasto);
+        this.contadorId++;
+        return this.gastos.get(id)
     }
-    actualizarGasto(gasto){
-        let id= parseInt(gasto.id)
-        const gastoEncontrado = this.buscarGasto(id);
-        if (gasto.id == gastoEncontrado.id ) {
-            let id = gastoEncontrado.id
-            this.listaGastos[id] = gasto
-            return gasto;
+    
+    calcularTotalSaldos() {
+        let totalSaldos = 0;
+        for (const saldos of this.saldos.values()) {
+            totalSaldos += saldos.monto;
         }
-        return null;  
+        return totalSaldos;
     }
-    eliminarGasto(id) {
-        let indiceGasto = this.buscarGasto(id);
-        if (indiceGasto.id !== -1) {
-            this.listaGastos.splice(indiceGasto, 1);
-            return indiceGasto;
-        } 
-        return -1;
+    calcularTotalGastos() {
+        let totalGastos = 0;
+        for (const gasto of this.gastos.values()) {
+            totalGastos += gasto.monto;
+        }
+        return totalGastos;
+    }
+    calcularDiferencia() {
+        const totalSaldos = this.calcularTotalSaldos();
+        const totalGastos = this.calcularTotalGastos();
+        return totalSaldos - totalGastos;
+    } 
+    actualizarGasto(id, nombre, nuevoMonto) {
+        if (this.gastos.has(id)) {
+            const gastoExistente = this.gastos.get(id);
+            gastoExistente.nombre = nombre;
+            gastoExistente.monto = nuevoMonto;
+            return gastoExistente;
+        }
+        return null
+    }   
+    borrarGasto(id) {
+        if (!this.gastos.has(id)) {
+            throw new Error('El gasto con el ID especificado no existe.');
+        }
+        this.gastos.delete(id);
+        return true;
     }
 }
-
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,13 @@
-const nuevoPresupuesto = new Presupuesto();
+const presupuesto = new Presupuesto();
 
 window.onload = () =>{    
     
-    const presupuestoFormulario = document.getElementById("presupuesto-form");
-    const presupuestoMonto = presupuestoFormulario.querySelector("#presupuesto-monto");
-    const presupuestoGuardar = presupuestoFormulario.querySelector("#presupuesto-boton");
+    // Saldos Formulario
+    const saldosFormulario = document.getElementById("presupuesto-form");
+    const saldosMonto = saldosFormulario.querySelector("#presupuesto-monto");
+    const saldosGuardar = saldosFormulario.querySelector("#presupuesto-boton");
     
+    // Gastos Formulario
     const gastosFormulario = document.getElementById("gastos-form");
     const gastosId = gastosFormulario.querySelector("#gastos-id");
     const gastosNombre = gastosFormulario.querySelector("#gastos-nombre");
@@ -13,151 +15,180 @@ window.onload = () =>{
     const gastosGuardar = gastosFormulario.querySelector("#gastos-guardar");
     const gastosActualizar = gastosFormulario.querySelector("#gastos-actualizar");
 
-    const presupuestoTotalTabla = document.getElementById("presupuesto-total");
+    // Tabla Totales
+    const saldosTotalTabla = document.getElementById("presupuesto-total");
     const gastosTotalTabla = document.getElementById("gastos-total");
     const diferenciaTotalTabla = document.getElementById("diferencia-total");
     
+    // Tabla Gastos 
     const gastosTabla = document.getElementById("compras-listado");
 
-    /*renderizarTotales
-     * Suma los presupuestos, gastos y calcula la diferencia entre ellos.
-     * Renderiza los totales en la tabla de totales.
-     */
-    function renderizarTotales(){
-        const totalPresupuesto = nuevoPresupuesto.sumarPresupuesto();
-        const totalGastos = nuevoPresupuesto.sumarGastos();
-        const diferencia = nuevoPresupuesto.calcularDiferencia();
-        presupuestoTotalTabla.textContent = totalPresupuesto 
+    const renderizarTotales =()=> {
+        const totalSaldos = presupuesto.calcularTotalSaldos();
+        const totalGastos = presupuesto.calcularTotalGastos();
+        const diferencia = presupuesto.calcularDiferencia();
+        saldosTotalTabla.textContent = totalSaldos 
         gastosTotalTabla.textContent = totalGastos;
         diferenciaTotalTabla.textContent = diferencia
         return console.log("renderizarTotales()::: OK")
     };
-    /*renderizarCompra()
-     * Lista todas las compras almacenadas en nuevoPresupuesto.listaGastos[]
-     * Usando createElement:
-     * - tr con id = gasto.id
-     * - td's con nombre,monto, y acciones borrar y editar
-     * botonBorrar()
-     * - Busca el gasto dentro de nuevoPresupuesto.listaGastos[id]
-     * - Borra el gasto dentro de listaGastos y el elemento tr del DOM
-     * botonEditar()
-     * - Captura el id del tr contenido, usando el metodo buscarGasto(id) compruebo que existe
-     * - Si es encontrado, captura tales datos y los renderiza en los input de gastos, junto con el id
-     */
-    function renderizarCompras(){
-        gastosTabla.innerHTML="";
-        const listado = nuevoPresupuesto.listaGastos;
-        listado.forEach( gasto => {
-            let id = gasto.id;
-            let nombre = gasto.nombre;
-            let monto = gasto.monto;
-            //row con id 
-            const trFila = document.createElement("tr")
-            trFila.setAttribute('id', id);
-            //td nombre
-            const tdNombre = document.createElement("td");
-            tdNombre.textContent = nombre;
-            trFila.appendChild(tdNombre);
-            //td monto
-            const tdMonto = document.createElement("td");
-            tdMonto.textContent = monto;
-            trFila.appendChild(tdMonto);
-            //td acciones
-            const tdAcciones = document.createElement("td");
-            //botonBorrar
-            const botonBorrar = document.createElement("button");
-            botonBorrar.textContent = "Borrar";
-            botonBorrar.setAttribute("id",`borrar-id-${id}`)
-            botonBorrar.classList.add( "btn", "btn-danger", "btn-sm","m-1", "boton-borrar");
-            //botonBorrar.addeventListener
-            botonBorrar.addEventListener("click", (event) => {
-                event.preventDefault();
-                let respuesta = nuevoPresupuesto.eliminarGasto(id);
-                if(respuesta !== -1){
-                    trFila.remove();
-                }else{
-                    console.log("eliminarGasto()::: NO ENCONTRADO") 
-                }
+
+    //contenedorBotones evalua si el click viene del botonBorrar o editar
+    gastosTabla.addEventListener("click", event => {
+        if (event.target.classList.contains("boton-borrar")) {
+            event.preventDefault();
+            const tr = event.target.closest("tr");
+            const id = tr.id;
+
+            let respuesta = presupuesto.borrarGasto(id);
+            if (respuesta) {
+                tr.remove();
                 renderizarTotales();
-                return console.log("botonBorrar::: OK")
-            });
-            tdAcciones.appendChild(botonBorrar);
-            //botonEditar
-            const botonEditar = document.createElement("button");
-            botonEditar.textContent = "Editar";
-            botonEditar.classList.add("btn", "btn-success", "btn-sm","m-1","boton-editar");
-            //botonEditar.addeventListener
-
-            const manejadorEditar =(evento)=>{
-                evento.preventDefault();
-                let row = evento.target.closest("tr");
-                let id = parseInt(row.id)
-                let gastoEncontrado = nuevoPresupuesto.buscarGasto(id)
-                if(gastoEncontrado == null){
-                    return console.log("buscarGasto()::: NO ENCONTRADO")
-                }
-                
-                console.log("buscarGasto()::: " + JSON.stringify(gastoEncontrado));
-                
-                let nombre = gastoEncontrado.nombre;
-                let monto = gastoEncontrado.monto;
-                gastosId.value = id;
-                gastosNombre.value = nombre;
-                gastosMonto.value = monto;
-                mostrarOcultarActualizar(event);
-                return console.log('botonEditar::: OK');
+                return console.log("botonBorrar::: OK");
+            } else {
+                return console.log("borrarBorrar()::: ERROR");
             }
-            
-            botonEditar.addEventListener("click",evento => manejadorEditar(evento) )
-
-            /*
-            botonEditar.addEventListener("click", (event) => {
-                event.preventDefault();
-                const tr = event.target.closest("tr");
-                let id = parseInt(tr.getAttribute("id") );
-                let gastoEncontrado = nuevoPresupuesto.buscarGasto(id)                
-                if(gastoEncontrado == null){
-                    return console.log("buscarGasto()::: NO ENCONTRADO")
+        } else if (event.target.classList.contains("boton-editar")) {
+            event.preventDefault();
+            const tr = event.target.closest("tr");
+            const id = tr.id;
+            console.log(id)
+            let gastoEncontrado = presupuesto.gastos.has(id);
+            console.log(gastoEncontrado)
+                try{    
+                    let nombre = gastoEncontrado.nombre;
+                    let monto = gastoEncontrado.monto;
+                    limpiarInput()
+                    gastosId.value = id;
+                    gastosNombre.value = nombre;
+                    gastosMonto.value = monto;
+                    manejadorGuardarActualizar(event);
+                    return console.log("botonEditar::: OK");
+                }catch(event){
+                    return console.error(error);
                 }
+            
+        }
+    });
 
-                console.log("buscarGasto()::: " + JSON.stringify(gastoEncontrado));
-                
-                let nombre = gastoEncontrado.nombre;
-                let monto = gastoEncontrado.monto;
-                gastosId.value = id;
-                gastosNombre.value = nombre;
-                gastosMonto.value = monto;
-                mostrarOcultarActualizar(event);
-                return console.log('botonEditar::: OK');
-            });
-            */
+    saldosGuardar.addEventListener("click", event => {
+        event.preventDefault();
+        let monto = saldosMonto.value
+        monto = parseInt(monto)
+        if( isNaN(monto) ){
+            return console.log('El monto del saldo debe ser mayor a 0.');
+        }
+        let montoAgregado = presupuesto.agregarSaldos(monto);
+        if (montoAgregado) {
+            try{
+                renderizarTotales();
+                limpiarInput();
+                return console.log(`saldoGuardar()::: OK`)
+            } catch(error){
+                return console.error(error)
+            }
+        }
+    });
 
-            // end botonEditar
-            //agrego el botonEditar a tdAcciones
-            tdAcciones.appendChild(botonEditar);
+    gastosGuardar.addEventListener("click", event => {
+        event.preventDefault();
+        let nombre = gastosNombre.value
+        let monto = parseInt(gastosMonto.value)
+        if( isNaN(monto) || nombre.trim().length === 0) {
+            return console.log('Nombre y/o Monto en Gasto con valor erroneo');
+        }
+        gastoAgregado = presupuesto.agregarGasto(nombre,monto);
+        console.log(gastoAgregado)
+        if (gastoAgregado){
+            try{
+                renderizarTotales();
+                renderizarGasto(gastoAgregado);
+                limpiarInput()
+                return console.log(`gastosGuardar::: OK`)
+            } catch(error){
+                return console.error(error)
+            }
+        }
+    });
 
-            trFila.appendChild(tdAcciones)
-            gastosTabla.appendChild(trFila)
-            console.log("renderizarCompra()::: OK")
+    gastosActualizar.addEventListener("click", event =>{
+        event.preventDefault();
+        let id = gastosId.value
+        let nombre = gastosNombre.value
+        let monto = parseInt(gastosMonto.value)
+        if( isNaN(monto) || nombre.trim().length === 0) {
+            return console.log('Nombre y/o Monto en Gasto con valor erroneo');
+        }
+        let gastoActualizado = presupuesto.actualizarGasto(id,nombre,monto);
+        if (gastoActualizado){
+            try{
+                renderizarTotales();
+                renderizarGasto(gastoActualizado);
+                limpiarInput();
+                manejadorGuardarActualizar(event)
+                return console.log(`gastosActualizar::: OK`)
+            } catch(error){
+                return console.error(error);
+            }
+        }    
+    })
 
-        })
+    const renderizarGasto =(gasto)=> {
+
+        const id = gasto.id
+        const nombre = gasto.nombre
+        const monto = gasto.monto
+        
+        //row con id 
+        const tr = document.createElement("tr")
+        tr.setAttribute('id', id);
+        
+        //td nombre
+        const tdNombre = document.createElement("td");
+        tdNombre.textContent = nombre;
+        tr.appendChild(tdNombre);
+        
+        //td monto
+        const tdMonto = document.createElement("td");
+        tdMonto.textContent = monto;
+        tr.appendChild(tdMonto);
+        
+        //td acciones
+        const tdAcciones = document.createElement("td");
+        
+        //botonBorrar
+        const borrar = document.createElement("button");
+        borrar.textContent = "Borrar";
+        borrar.classList.add(
+            "btn","btn-danger","btn-sm",
+            "m-1", "boton-borrar"
+        );    
+        tdAcciones.appendChild(borrar);
+        
+        //botonEditar
+        const editar = document.createElement("button");
+        editar.textContent = "Editar";
+        editar.classList.add(
+            "btn", "btn-success", "btn-sm",
+            "m-1","boton-editar"
+        );
+        tdAcciones.appendChild(editar);
+
+        tr.appendChild(tdAcciones)
+        gastosTabla.appendChild(tr)
+        console.log("renderizarGasto()::: OK")
+
     };
-    /*limpiarInput()
-     * Captura todos los input del DOM y los inicializa en = ''
-     */
-    function limpiarInput(){
+
+    const limpiarInput =()=> {
         const input = document.querySelectorAll('input')
         input.forEach( (elemento) => {
             elemento.value = "";
         })
         return console.log("limpiarInput()::: OK")
     };
-    /*mostrarOcultarActualizar(event)
-     * Evalua si está siendo invocada desde editar o no.
-     * Si es asi, permite actualizar valores.
-     * Si no, devuelve los input y botones por defecto.
-     */
-    function mostrarOcultarActualizar(event){
+
+    const manejadorGuardarActualizar =(event)=> {
         if(event.target.classList.contains("boton-editar")){
             gastosId.parentElement.classList.remove("d-none")
             gastosGuardar.parentElement.classList.add("d-none");
@@ -174,65 +205,5 @@ window.onload = () =>{
         }
         
     };
-    /*presupuestoGuardar.addEventListener()
-     * guarda los valores ingresados en el input de presupuesto
-     */
-    presupuestoGuardar.addEventListener("click", event => {
-        event.preventDefault();
-        let monto = presupuestoMonto.value
-        monto = parseInt(monto)
-        if( isNaN(monto) ){
-            return console.log('monto en Gasto con valor erroneo::: ' + monto);
-        }
-        montoAgregado = nuevoPresupuesto.agregarPresupuesto(monto);
-        totalPresupuesto = nuevoPresupuesto.sumarPresupuesto();
-        nuevoPresupuesto.ultimoPresupuestoTotal = totalPresupuesto;
-        renderizarTotales();
-        limpiarInput();
-        return console.log(`presupuestoGuardar::: OK`)
-    });
-    /*gastosGuardar.addEventListener()
-     * guarda los valores ingresados en los input de gastos
-     */
-    gastosGuardar.addEventListener("click", event => {
-        event.preventDefault();
-        let nombre = gastosNombre
-        let monto = gastosMonto;
-        nombre = String(nombre.value);
-        monto = parseInt(monto.value);
-        if( isNaN(monto) || nombre.trim() == null) {
-            return console.log('Nombre y/o Monto en Gasto con valor erroneo');
-        }
-        gastoAgregado = nuevoPresupuesto.agregarGasto(nombre,monto);
-        limpiarInput()
-        renderizarTotales();
-        renderizarCompras();
-        return console.log(`gastosGuardar::: OK`)
-    });
-    /*gastosActualizar.addEventListener()
-     * guarda los valores de los input ingresados en gastos, cuando se van a editar los ya ingresados.
-     */
-    gastosActualizar.addEventListener("click", event => {
-        event.preventDefault();
-        let id = gastosId.value
-        let nombre = gastosNombre.value
-        let monto = gastosMonto.value;
-        if ( isNaN(monto) || (nombre.trim() === "") ){
-            return console.log('valores invalidos en input nombre y/o monto.')
-        }
-        const gasto = {
-            id : id,
-            nombre : nombre,
-            monto : parseInt(monto)
-        }
-        let gastoActualizado = nuevoPresupuesto.actualizarGasto(gasto);
-        if(gastoActualizado == null){
-            return console.log("error no encontrado en actualizarGasto")
-        }
-        limpiarInput()
-        renderizarCompras();
-        renderizarTotales();
-        mostrarOcultarActualizar(event)
-        return console.log("gastoActualizado::: " + JSON.stringify(gastoActualizado))
-    });
-};
+
+}
